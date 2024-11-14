2f0c7fb934709842ce9ac666519a40e5107496b0
Fabricio Ryu <fabricio.akashi26@gmail.com>
2024-11-14 14:24:42 -0300

    Update on CV

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function downloadPDF() {
     const link = document.createElement('a');
-    link.href = 'https://www.fabricioakashi.site/assets/FabricioAkashiCV.pdf';
+    link.href = 'https://www.fabricioakashi.site/assets/Curriculo.pdf';
     link.download = 'Fabricio-CV.pdf';
     link.click();
 }
